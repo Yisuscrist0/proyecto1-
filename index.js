6f17e1f5cb9ccab1c3a7f9de73bde7dc06529783
Ninja-Antonio <157515579+Ninja-Antonio@users.noreply.github.com>
2024-01-23 19:11:58 +0000

    feat: arrancar el programa

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 console.log("Esta es una calculadora");
+
+args = process.argv.slice(2);
+// Parametro 1: tipo de operacion; suma, resta, etc.
+// Parametro 2: num1
+// Parametro 3: num2
+
+function isNumeric(value) {
+    return /^\d+$/.test(value);
+  }
+
+if (args.length == 3) {
+    
+    for(i = 1; i < 3; i++){
+        if(!isNumeric(args[i])){
+            console.log(`No es un numero: ${args[i]}`);
+        }
+    }
+
+
+    // Aqui ya puedo escribir la calculadora
+
+    
+
+}
