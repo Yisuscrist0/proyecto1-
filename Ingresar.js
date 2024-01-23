42579be985ba9766af1a3148fa31454f0180af3f
Ninja-Antonio <157515579+Ninja-Antonio@users.noreply.github.com>
2024-01-23 18:36:10 +0000

    chore: refactorizar codigo

--- Ingresar.js
+++ Ingresar.js
@@ -1,7 +1,11 @@
-var num1, num2, resultado=0;
- num1 = prompt("Ingresa un primer numero");
 
- num2 = prompt("Ingresa un segundo numero");
+function requestNumbers(){
+    var num1, num2, resultado=0;
+    num1 = prompt("Ingresa un primer numero");
+   
+    num2 = prompt("Ingresa un segundo numero");
+   
+    document.write('este es num1',num1);
+    document.write('este es num2',num2);
+}
 
- document.write('este es num1',num1);
- document.write('este es num2',num2);
