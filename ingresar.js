f354813bc0d6b34a9f356a91743eb51505fec9ff
Yisuscrist0 <157408296+Yisuscrist0@users.noreply.github.com>
2024-01-31 17:36:23 +0000

    feat: no resuelto aun

--- ingresar.js
+++ ingresar.js
@@ -1,10 +1,8 @@
-ingresar = function(){
-    var num1, num2;
+ingresa = function (num1, num2) {
     num1 = prompt("Ingresa un primer numero");
-   
-    num2 = prompt("Ingresa un segundo numero");
-   
-    return [num1, num2]; // Arreglo [0=>num1, 1 => num2]
-}
 
-export default ingresar;
+    num2 = prompt("Ingresa un segundo numero");
+
+    return [num1, num2]; // Arreglo [0=>num1, 1 => num2]
+};
+export default ingresa;
