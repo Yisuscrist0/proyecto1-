795b7d4f0cac0c652ebdb10c5cfa7942775d3668
Jordan <149917273+WendyLaFurra@users.noreply.github.com>
2024-02-15 18:41:21 +0000

    chore: avance

--- servi.js
+++ servi.js
@@ -5,7 +5,11 @@ const app = express();
 app.get('/', (req, res) =>{
     res.sendFile('./static/index.html', {
         root: __dirname
-    })
-})
+    });
+});
+
+app.get('/calculadora',(req, res)=>{
+    res.send(`Numero 1: ${req.query.num1}, Numero 2: ${req.query.num2}, Opción: ${req.query.opcion}`);
+});
 app.listen(3000);
 console.log('Server on port ${3000}');
